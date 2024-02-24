f31d1ee53ff642a4bd32b1a0c071352abc360bd8
ravvi1104 <rry4298@gmail.com>
2024-02-24 18:06:21 +0530

    update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,10 +2,16 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer>
+    <footer style={footerStyle}>
       <p>&copy; 2024 Bluetick Consultants. All rights reserved.</p>
     </footer>
   );
 }
 
 export default Footer;
+
+const footerStyle = {
+  backgroundColor: '#fff',
+  textAlign: 'center',
+  padding: '1px',
+};
