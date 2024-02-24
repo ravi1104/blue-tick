fba7e820a757557aa0c7ed1acc520201d54dc9c2
ravvi1104 <rry4298@gmail.com>
2024-02-24 19:19:22 +0530

    update all files (working);

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React from 'react';
-import HeroSection from './components/HeroSection';
-import WhatWeOffer from './components/WhatWeOffer';
-import Blogs from './components/Blogs';
-import LetsConnect from './components/LetsConnect';
-import Footer from './components/Footer';
-import './App.css'; // Import your CSS file for styling
+import HeroSection from './components/heroSection/HeroSection';
+import WhatWeOffer from './components/whatWeOffer/WhatWeOffer';
+import Blogs from './components/blogs/Blogs';
+import LetsConnect from './components/letsConnect/LetsConnect';
+import Footer from './components/footer/Footer';
+import './App.css';
+import Navbar from './components/Navbar/Navbar';
+import '@fortawesome/fontawesome-free/css/all.css';
 
 const App = () => {
   return (
     <div className="App">
+      <Navbar/>
       <HeroSection />
+
       <WhatWeOffer />
+      <h1 className='heading'>Explore and enjoy the blogs written by our passionate techies <i class="fa-brands fa-microblog"></i></h1>
       <Blogs />
+      <h1 className='heading'> Let's Connect <i class="fa-regular fa-address-card"></i></h1>
+      
       <LetsConnect />
       <Footer />
     </div>
